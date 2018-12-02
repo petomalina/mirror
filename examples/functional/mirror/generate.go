1ffc99e6757e6af2a6f86915b26705ce0c926a29
Peter Malina <peter.malina@flowup.cz>
2018-12-02 18:51:23 +0100

    bundle: added utility functions to automatically run the bundle

--- examples/functional/mirror/generate.go
+++ examples/functional/mirror/generate.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"github.com/petomalina/mirror"
-	"github.com/urfave/cli"
 	"log"
-	"os"
 	"path/filepath"
 	"strings"
 )
@@ -24,43 +22,24 @@ func (us _T_Slice) Map(cb _T_MapCallback) _T_Slice {
 `
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "functional-generator"
-	app.Usage = "generates map/filter/reduce for input models"
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "pkg, p",
-			Value: ".",
-			Usage: "Package to be used for model determination",
-		},
-		cli.StringSliceFlag{
-			Name:  "models, m",
-			Usage: "Models that should be considered when generating",
-		},
-		cli.StringFlag{
-			Name:  "out, o",
-			Value: ".",
-			Usage: "Directory for the generated files to be saved in",
-		},
-	}
-	app.Action = func(c *cli.Context) error {
-		bundle := &mirror.Bundle{}
-		return bundle.Run("", []string{}, "", func(outDir string, models []interface{}) error {
-			out := mirror.File(filepath.Join(outDir, "functional.go"))
-			blocks := []string{}
+	err := mirror.RunDefaultApp("mirror-functional", &mirror.Bundle{
+		RunFunc: ProcessModel,
+	})
 
-			for _, m := range models {
-				str := mirror.ReflectStruct(m)
-
-				blocks = append(blocks, strings.Replace(mapTemplate, "_T_", str.Name(), -1))
-			}
-
-			return out.Write(blocks...)
-		})
-	}
-
-	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func ProcessModel(outDir string, models []interface{}) error {
+	out := mirror.File(filepath.Join(outDir, "functional.go"))
+	blocks := []string{}
+
+	for _, m := range models {
+		str := mirror.ReflectStruct(m)
+
+		blocks = append(blocks, strings.Replace(mapTemplate, "_T_", str.Name(), -1))
+	}
+
+	return out.Write(blocks...)
 }
