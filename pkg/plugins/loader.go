8ad4d350faf599bdbe3154eeb6eee6b209b117e2
Peter Malina <peter.malina@flowup.cz>
2018-12-25 10:58:57 +0100

    plugin: now supports whole lifecycle

---
+++ pkg/plugins/loader.go
@@ -0,0 +1,58 @@
+package plugins
+
+import "os"
+
+// Loader encapsulates full lifecycle of a plugin
+type Loader struct {
+	// TargetPath is a relative path to the plugin that should be built
+	TargetPath string
+	// PreserveCache determines if the copied cached files should be preserved
+	// at the end of the lifecycle
+	PreserveCache bool
+	// GenerateSymbols automatically generates symbols for desired types,
+	// e.g. type User struct {} will have his var XUser User generated
+	// in case it is passed into symbolNames for Load function
+	GenerateSymbols bool
+}
+
+func (l *Loader) Load(symbolNames []string) ([]interface{}, error) {
+	pkg, err := FindPackage(l.TargetPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// copy everything into the cache so we can manipulate it further and avoid caching
+	cacheTargetPath, err := CopyPackageToCache(pkg, DefaultCache)
+	if err != nil {
+		return nil, err
+	}
+
+	// generate symbols and accept new symbol names
+	if l.GenerateSymbols {
+		symbolNames, err = GenerateSymbolsForModels(symbolNames, cacheTargetPath)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	so, err := Build(cacheTargetPath, cacheTargetPath)
+	if err != nil {
+		return nil, err
+	}
+
+	syms, err := LoadSymbols(so, symbolNames)
+	if err != nil {
+		return nil, err
+	}
+
+	// don't do cleanup if we want to preserve the cache, just return
+	if l.PreserveCache {
+		return syms, nil
+	}
+
+	return syms, os.RemoveAll(cacheTargetPath)
+}
+
+func (l *Loader) Watch(symbolnames []string) (chan<- []interface{}, error) {
+	return nil, nil
+}
